7d82dd69e959139c291aaf317595c0cf861056a0
Matti Niemenmaa <matti.niemenmaa+github@iki.fi>
2010-11-01 15:48:21 +0200

    Minimaaliset lippu-, vuoro-, juna- ja asemamallit.

---
+++ devsite/minivr/admin.py
@@ -0,0 +1,7 @@
+import minivr.models as minivr
+from django.contrib import admin
+
+admin.site.register(minivr.Lippu)
+admin.site.register(minivr.Vuoro)
+admin.site.register(minivr.Juna)
+admin.site.register(minivr.Asema)
--- devsite/minivr/models.py
+++ devsite/minivr/models.py
@@ -1,8 +1,42 @@
+# coding=utf-8
+
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Tehty Djangon tutoriaalin ohjeen mukaan
-# http://docs.djangoproject.com/en/dev/intro/tutorial01/
+class Lippu(models.Model):
+    vuoro = models.ForeignKey('Vuoro')
+    hinta = models.DecimalField(max_digits = 4, decimal_places = 2)
 
-class Poikkeustiedote(models.Model):
-    eff_date = models.DateTimeField('poikkeuksen vaikutusaika')
-    message = models.CharField(max_length=1000)
+    def __unicode__(self):
+        return unicode(self.vuoro) + " " + unicode(self.hinta)
+
+class Vuoro(models.Model):
+    juna = models.ForeignKey('Juna')
+
+    # Jokaisella asemalla ei välttämättä pysähdytä, joten kaikki tulee listata.
+    asemat = models.ManyToManyField('Asema')
+
+    aika             = models.TimeField()
+    paikkoja_vapaana = models.PositiveIntegerField()
+
+    def clean(self):
+        if self.paikkoja_vapaana > self.juna.paikkoja:
+            raise ValidationError('Vuorossa ei voi olla enemmän vapaita '+\
+                                  'paikkoja kuin junassa on tilaa.')
+
+    def __unicode__(self):
+        return unicode(self.juna) + " " + unicode(self.aika)
+
+class Juna(models.Model):
+    tyyppi   = models.CharField(max_length = 255)
+    paikkoja = models.PositiveIntegerField()
+
+    def __unicode__(self):
+        return self.tyyppi
+
+class Asema(models.Model):
+    nimi     = models.CharField(max_length = 255)
+    yhteydet = models.ManyToManyField('self')
+
+    def __unicode__(self):
+        return self.nimi
