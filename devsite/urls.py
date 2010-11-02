31cf3cf63acaa6afb7850baf495e902c1c3ec042
Matti Niemenmaa <matti.niemenmaa+github@iki.fi>
2010-11-02 22:51:51 +0200

    Simplify urlpatterns specification.

--- devsite/urls.py
+++ devsite/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    (r'^minivr/$',                                     'minivr.views.index'),
-    (r'^minivr/service/(?P<service_id>\d+)/$',         'minivr.views.service_detail'),
-    (r'^minivr/service/(?P<service_id>\d+)/reserve/$', 'minivr.views.service_reserve'),
+urlpatterns = patterns('minivr.views',
+    (r'^minivr/$',                                     'index'),
+    (r'^minivr/service/(?P<service_id>\d+)/$',         'service_detail'),
+    (r'^minivr/service/(?P<service_id>\d+)/reserve/$', 'service_reserve'))
 
+urlpatterns += patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
 )
