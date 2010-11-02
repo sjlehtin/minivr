a6efef45bac0cff2efdfeac570321bd24d14f024
Matti Niemenmaa <matti.niemenmaa+github@iki.fi>
2010-11-02 15:40:56 +0200

    Add some trivial UI for reserving a ticket.

--- devsite/minivr/views.py
+++ devsite/minivr/views.py
@@ -1,17 +1,32 @@
-from django.db.models import Min
-from django.shortcuts import render_to_response, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.db.models         import Min
+from django.http              import HttpResponseRedirect
+from django.shortcuts         import render_to_response, get_object_or_404
+from django.template          import RequestContext
 
 from minivr.models import Service
 
-def index(request):
+def index(request, last_reserved = None):
     services = Service.objects.\
                    filter(free_seats__gt = 0).\
                    annotate(departure_time = Min('schedule__departure_time')).\
                    order_by('departure_time')
-    return render_to_response('minivr/index.html', {'services':services})
+    if last_reserved:
+        last_reserved = int(last_reserved)
+    return render_to_response(
+        'minivr/index.html',
+        {'services':services, 'last_reserved':last_reserved},
+        context_instance = RequestContext(request))
 
 def service_detail(request, service_id):
     service = get_object_or_404(Service, id = service_id)
     stops = service.schedule.order_by('departure_time')
     return render_to_response('minivr/service_detail.html',
                               {'service':service, 'stops':stops})
+
+def service_reserve(request, service_id):
+    service = get_object_or_404(Service, id = service_id)
+    service.free_seats -= 1
+    url = reverse('minivr.views.index', args = (service_id,))
+    service.save()
+    return HttpResponseRedirect(url)
