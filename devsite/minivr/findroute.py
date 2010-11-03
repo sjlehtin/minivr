f76caea95a46fea4217bf2a80aaa815bada7c5bf
Sami J. Lehtinen <sjl@iki.fi>
2010-11-03 04:37:17 +0200

    Working shortest path algorithm.  Added tests.

--- devsite/minivr/findroute.py
+++ devsite/minivr/findroute.py
@@ -1,25 +1,23 @@
-(DEPARTURE, ARRIVAL) = range(0:1)
+import unittest
 
-#
-## Misc notes.
-#
-# Maximum exchange time (the window for additional routes) is 24 hours.
-# Graph generated mostly during execution, perhaps cached.
+(DEPARTURE, ARRIVAL) = range(0, 2)
 
-class TestNode(object):
-    def __init__(self, station):
-        self.is_visited = False
-        self.station = station
-        self.name = station.get_name()
+class GraphNode(object):
+    def __init__(self, id):
+        self.id = id
+        self.connections = []
 
-    def visited(self):
-        return is_visited
+    def add_connection(self, neighbor, cost):
+        self.connections.append((neighbor, cost))
 
+    def get_connections(self):
+        return self.connections[:]
+        
     def __hash__(self):
-        return hash(self.name)
+        return hash(self.id)
 
     def __cmp__(self, other):
-        return cmp(self.name, other.name)
+        return cmp(self.id, other.id)
 
 def get_route(from_station, to_station, 
               all_stations,
@@ -28,26 +26,50 @@ def get_route(from_station, to_station,
     """Get "shortest path" between two stations.  The weighting between
     two vertices is done with a function."""
 
-    all_stations = all_stations[:]
+    if from_station not in all_stations:
+        raise ValueError("departure station must be in set of all stations")
+    if to_station not in all_stations:
+        raise ValueError("destination station must be in set of all stations")
 
     distances = {}
     previous = {}
     distances[from_station] = 0
 
-    while all_stations:
-        cur = sorted(distances.iteritems(), 
-                     key=lambda x: x[1] (if x.visited else sys.maxint))[0]
-        if cur[1] == sys.maxint:
+    stations = set(all_stations)
+    while stations:
+        def get_closest_node():
+            candidates = filter(lambda x: x[0] in stations, 
+                                distances.iteritems())
+            if not candidates:
+                return None
+            closest_nodes = [nn[0] for nn in 
+                             sorted(candidates, key=lambda x: x[1])]
+            return closest_nodes[0]
+
+        cur = get_closest_node()
+        if not cur:
             raise ValueError("no path from %s to %s" % (from_station, 
                                                         to_station))
-        del all_stations[cur]
+        # The next closest node is the target node.
+        if cur == to_station:
+            break
 
-        for neighbor in cur.get_connections():
-            new_distance = distances[cur] + get_distance(cur, neighbor)
-            if new_distance < distances[neighbor]:
+        stations.remove(cur)
+
+        for (neighbor, distance) in cur.get_connections():
+            new_distance = distances[cur] + distance
+            if ((not neighbor in distances) 
+                or (new_distance < distances[neighbor])):
                 distances[neighbor] = new_distance
-                previous[neighbor] = cur
+                previous[neighbor] = cur    
+        
+    route = [to_station]
+    cur = to_station
+    while (cur in previous):
+        cur = previous[cur]
+        route.insert(0, cur)
 
+    return route
 
 #  function Dijkstra(Graph, source):
 #     for each vertex v in Graph:           // Initializations
@@ -73,3 +95,131 @@ def get_route(from_station, to_station,
 #     end while ;
 #     return dist[] ;
 # end Dijkstra.
+
+class SimpleLinearTestCase(unittest.TestCase):
+    def testRouteToSelf(self):
+        a = GraphNode("first")
+        route = get_route(a, a, [a])
+        self.assertEqual(route, [a])
+
+    def testRouteToNeighbor(self):
+        a = GraphNode("first")
+        b = GraphNode("second")
+        a.add_connection(b, 1)
+        b.add_connection(a, 1)
+        route = get_route(a, b, [a, b])
+        self.assertEqual(route, [a, b])
+
+    def testRouteToEnd(self):
+        a = GraphNode("first")
+        b = GraphNode("second")
+        c = GraphNode("third")
+        a.add_connection(b, 1)
+        b.add_connection(a, 1)
+        b.add_connection(c, 1)
+        c.add_connection(b, 1)
+        route = get_route(a, c, [a, b, c])
+        self.assertEqual(route, [a, b, c])
+
+    def testRouteFromBeginningToMiddle(self):
+        a = GraphNode("first")
+        b = GraphNode("second")
+        c = GraphNode("third")
+        a.add_connection(b, 1)
+        b.add_connection(a, 1)
+        b.add_connection(c, 1)
+        c.add_connection(b, 1)
+        route = get_route(a, b, [a, b, c])
+        self.assertEqual(route, [a, b])
+
+    def testRouteFromMiddleToEnd(self):
+        a = GraphNode("first")
+        b = GraphNode("second")
+        c = GraphNode("third")
+        a.add_connection(b, 1)
+        b.add_connection(a, 1)
+        b.add_connection(c, 2)
+        c.add_connection(b, 2)
+        route = get_route(b, c, [a, b, c])
+        self.assertEqual(route, [b, c])
+
+    def testMustFailConnectionNotInSet(self):
+        a = GraphNode("first")
+        b = GraphNode("second")
+        c = GraphNode("third")
+        a.add_connection(b, 1)
+        b.add_connection(a, 1)
+        b.add_connection(c, 2)
+        c.add_connection(b, 2)
+        
+        self.assertRaises(ValueError, get_route, a, c, [a, c])
+
+class SmallGraphTestCase(unittest.TestCase):
+    def setUp(self):
+        a = GraphNode("a")
+        b = GraphNode("b")
+        c = GraphNode("c")
+        d = GraphNode("d")
+        e = GraphNode("e")
+        f = GraphNode("f")
+        g = GraphNode("g")
+        a.add_connection(d, 1)
+        d.add_connection(a, 1)
+
+        b.add_connection(d, 2)
+        d.add_connection(b, 2)
+
+        c.add_connection(d, 2)
+        d.add_connection(c, 2)
+
+        d.add_connection(e, 4)
+        e.add_connection(d, 4)
+
+        d.add_connection(f, 3)
+        f.add_connection(d, 3)
+
+        e.add_connection(g, 1)
+        g.add_connection(e, 1)
+
+        f.add_connection(g, 3)
+        g.add_connection(f, 3)
+
+        self.all_nodes = [a, b, c, d, e, f, g]
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+        self.e = e
+        self.f = f
+        self.g = g
+
+    def testBasic(self):
+        self.assertEqual(get_route(self.a, self.b, self.all_nodes), 
+                         [self.a, self.d, self.b])
+
+        self.assertEqual(get_route(self.a, self.e, self.all_nodes), 
+                         [self.a, self.d, self.e])
+
+        self.assertEqual(get_route(self.a, self.f, self.all_nodes), 
+                         [self.a, self.d, self.f])
+
+        self.assertEqual(get_route(self.f, self.g, self.all_nodes), 
+                         [self.f, self.g])
+
+        self.assertEqual(get_route(self.d, self.g, self.all_nodes), 
+                         [self.d, self.e, self.g])
+
+        self.assertEqual(get_route(self.e, self.f, self.all_nodes), 
+                         [self.e, self.g, self.f])
+
+        self.assertEqual(get_route(self.a, self.g, self.all_nodes), 
+                         [self.a, self.d, self.e, self.g])
+
+        self.assertEqual(get_route(self.g, self.a, self.all_nodes), 
+                         [self.g, self.e, self.d, self.a])
+
+class LargeGraphTestCase(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
