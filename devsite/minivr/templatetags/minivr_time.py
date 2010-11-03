0ca5f3ac02d4e646deb04ed5f45886f49d0f64da
Sami J. Lehtinen <sjl@iki.fi>
2010-11-03 23:16:53 +0200

    Removed unnecessary line-end backslashes to improve readability a bit.

--- devsite/minivr/templatetags/minivr_time.py
+++ devsite/minivr/templatetags/minivr_time.py
@@ -7,6 +7,5 @@ register = template.Library()
 @register.filter
 def addminutes(time, minutes):
     return (datetime(year = 2000, month = 1, day = 1,
-                     hour = time.hour, minute = time.minute) \
-            + timedelta(minutes = minutes)) \
-        .time()
+                     hour = time.hour, minute = time.minute)
+            + timedelta(minutes = minutes)).time()
