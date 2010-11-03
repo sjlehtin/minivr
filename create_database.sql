f34ba171e92c630f90d669099446edef684477d3
Sami J. Lehtinen <sjl@iki.fi>
2010-11-03 04:47:54 +0200

    My postgres complained about mismatching locales, so changed LC_CTYPE to use plain "UTF-8".  Hopefully works for Linux too.

--- create_database.sql
+++ create_database.sql
@@ -1,3 +1,3 @@
 -- PostgreSQL database and user initialization
 CREATE USER minivr UNENCRYPTED PASSWORD 'minivr345';
-CREATE DATABASE minivr ENCODING 'UTF-8' LC_CTYPE 'fi_FI.UTF-8';
+CREATE DATABASE minivr ENCODING 'UTF-8' LC_CTYPE 'UTF-8';
